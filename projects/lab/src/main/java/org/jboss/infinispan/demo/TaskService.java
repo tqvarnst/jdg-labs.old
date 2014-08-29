165a0e8a8541437acf030c6c62fd0e66afeaee62
Thomas Qvarnstrom <tqvarnst@redhat.com>
2014-08-29 14:29:21 +0200

    Fix for transaction issues with updateing Tasks

--- projects/lab/src/main/java/org/jboss/infinispan/demo/TaskService.java
+++ projects/lab/src/main/java/org/jboss/infinispan/demo/TaskService.java
@@ -74,8 +74,9 @@ public class TaskService {
 
 	
 	public void update(Task task) {
-		em.merge(task);
-		cache.replace(task.getId(),task);
+		Task newTask = em.merge(task);
+		em.detach(newTask);
+		cache.replace(task.getId(),newTask);
 	}
 	
 	@PostConstruct
